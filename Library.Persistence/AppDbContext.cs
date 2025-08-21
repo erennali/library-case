0b80e39ead710856cfa49cc764b64b32999a54a2
erennali <eren_ali_koca@hotmail.com>
2025-08-21 09:50:55 +0300

    feat: install ef core lib

--- Library.Persistence/AppDbContext.cs
+++ Library.Persistence/AppDbContext.cs
@@ -1,6 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+
 namespace Library.Persistence;
 
-public class AppDbContext
+public class AppDbContext : DbContext
 {
-    
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
 }
